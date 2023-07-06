4ab56fb6e1941c33896bd68668a49dad8d17eace
lham72 <lham@hotmail.fr>
2023-07-06 14:27:44 +0200

    docker compose with mongo container and myapp container working together allright

--- app.py
+++ app.py
@@ -1,32 +1,56 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 
- 
+application = Flask(__name__)
 
-app = Flask(__name__)
+application.config["MONGO_URI"] = 'mongodb://host.Docker.internal:27017/mymongodb'
 
- 
+mongo = PyMongo(application)
+db = mongo.db
 
-# Liste des utilisateurs (données fictives pour cet exemple)
+# Liste des utilisateurs de base (données fictives pour cet exemple)
 users = [
     {'id': 1, 'name': 'Alice', 'email': 'alice@example.com'},
     {'id': 2, 'name': 'Bob', 'email': 'bob@example.com'},
     {'id': 3, 'name': 'Charlie', 'email': 'charlie@example.com'}
-]
+    ]
 
- 
+#route test
+@application.route('/bonjour', methods=['GET'])
+def hello_world():
+    return 'Hello World!'
 
-@app.route('/')
-def home():
-    return "Bonjour"
+# print all database, the READ of CRUD
+@application.route('/showusers', methods=['GET'])
+def get_users():
 
- 
-@app.route('/showuser')
-def show():
-    return "au revoir !"
+    users = db.users.find()
+
+    user_list = []
+
+    for user in users:
+
+        user_list.append({'id': user['id'], 'name': user['name'], 'email': user['email']})
+
+    return jsonify({'users': user_list})
+
+@application.route('/newuser', methods=['POST'])
+def create_user():
+
+    user_data = request.get_json()
+
+    db.users.insert_one(user_data)
+
+    return jsonify({'message': 'User created successfully'})
 
 
-
- 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+
+    # Insertion des données dans la base de données
+    for user in users:
+        db.users.insert_one(user)
+        
+    application.run(host='0.0.0.0', port=5000, debug=True)
